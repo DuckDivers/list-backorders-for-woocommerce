3d512c1c10f5c6a2417717078cff1e6642ccb564
DuckDivers <howarde@scubaboard.com>
2017-03-18 15:40:39 -0400

    Add files via upload

---
+++ woocommerce-list-backorders.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  	Plugin Name: Woocommerce List Backorders
+  	Plugin URI: http://www.duckdiverllc.com/
+  	Version: 1.0
+  	Author: HowardE
+  	Description: Get a list of backordered items and orders with backorders from the Woocommerce Tab.
+	License:           GNU General Public License v3
+	License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ */
+if ( ! defined( 'ABSPATH' ) )
+exit; 
+
+add_action('admin_menu', 'register_backorder_page');
+
+function register_backorder_page() {
+    add_submenu_page( 'woocommerce', 'View Backorders List | By Duck Diver', 'List Backorders', 'manage_options', 'manage-backorder', 'manage_backorder_callback' ); 
+}
+
+function manage_backorder_callback() {
+    include("backorder.php");
+}
